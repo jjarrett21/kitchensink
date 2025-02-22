087da31eedc27414bfe0826f88852920ae2e88e3
jjarrett21 <jjarrett21@gmail.com>
2025-02-22 16:17:14 -0500

    fixes for css imports, vite config, and adds axios api scaffolding

--- index.js
+++ index.js
@@ -23,19 +23,18 @@ try {
   process.exit(1);
 }
 
-
 console.log(
   "Installing dependencies: Tailwind, react-query, react-router, and others..."
 );
 execSync(
-  `npm install tailwindcss postcss autoprefixer @tanstack/react-query axios react-router react-router-dom @tailwindcss/vite zod ${customPackages}`,
+  `npm install tailwindcss postcss autoprefixer @tanstack/react-query axios react-router react-router-dom @tailwindcss/vite zod qs ${customPackages}`,
   { stdio: "inherit" }
 );
 
 console.log("Installing dev dependencies: vitest... and more");
 
 execSync(
-  "npm install -D vitest jsdom test-utils @testing-library/react @testing-library/jest-dom @testing-library/user-event",
+  "npm install -D vitest jsdom test-utils @testing-library/dom @testing-library/react @testing-library/jest-dom @testing-library/user-event @types/qs @vitejs/plugin-react",
   { stdio: "inherit" }
 );
 
@@ -58,33 +57,16 @@ fs.writeFileSync(
 
 console.log("Updating Vite config...");
 const viteConfigPath = path.join(projectPath, "vite.config.ts");
-const viteConfig = fs.readFileSync(viteConfigPath, "utf-8");
-const updatedViteConfig = viteConfig.replace(
-  ``,
-  `/// <reference types="vitest" />
+const viteConfigContent = fs.readFileSync(viteConfigPath, "utf-8");
+const updatedViteConfig = `
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  /** @see https://vitest.dev/config/ */
-  test: {
-    coverage: {
-      include: ["src/**/*.ts", "src/**/*.tsx"],
-
-      reporter: ["html", "text-summary", "lcov"],
-      provider: "istanbul",
-    },
-    reporters: ["default"],
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./setup.ts",
-  },
-});
-    `
-);
+${viteConfigContent}
+`;
 fs.writeFileSync(viteConfigPath, updatedViteConfig);
 
 console.log("Updating main.tsx with Providers...");
@@ -109,6 +91,31 @@ createRoot(document.getElementById("root")!).render(
     </Router>
   </StrictMode>
 );
+`
+);
+
+console.log("Creating api folder and client.ts...");
+const apiFolderPath = path.join(projectPath, "src", "api");
+const clientTsPath = path.join(apiFolderPath, "client.ts");
+
+if (!fs.existsSync(apiFolderPath)) {
+  fs.mkdirSync(apiFolderPath);
+}
+
+fs.writeFileSync(
+  clientTsPath,
+  `import axios from "axios";
+import qs from "qs";
+
+const api = axios.create({
+  baseURL: "VITE_BASE_API_URL",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "comma" }),
+});
+
+export { api };
 `
 );
 
